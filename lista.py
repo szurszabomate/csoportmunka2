3531e4497640ee711faef30ee9f935d2d5274544
Szürszabó <szurszabomate100@gmail.com>
2022-11-22 13:38:17 +0100

    asa

--- lista.py
+++ lista.py
@@ -1,6 +1,17 @@
-szamok = ["egy","kettő","három","négy"]
-szamok.append("öt")
-print(szamok)
+lista=[1,2,3,4,5]
+lista2=[6,7,8,9,10]
 
-print("Az első szám az", szamok[0])
-print("A második szám a", szamok[1])
+x = lista+lista2
+print(x)
+
+a = lista[2]*lista2[1]
+print(a)
+
+b = lista[2]+lista2[1]
+print(b)
+
+c = lista2[2]-lista[1]
+print(c)
+
+d = lista2[2]//lista[1]
+print(d)
